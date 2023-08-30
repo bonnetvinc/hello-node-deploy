aa8c65d6bf61e01ce9e902b793ca9bbd7e0d5bf8
Vincent <vincent.vbonnet@gmail.com>
2023-08-30 00:19:56 -0400

    w

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 
 import express from "express";
-import cors from "cors";
-import helmet from "helmet";
-import { itemsRouter } from "./items/items.router";
+// import cors from "cors";
+// import helmet from "helmet";
+// import { itemsRouter } from "./items/items.router";
 // import { errorHandler } from "./middleware/error.middleware";
 // import { notFoundHandler } from "./middleware/not-found.middleware";
 
@@ -22,8 +22,8 @@ app.get('/', (req, res) => {
  *  App Configuration
  */
 
-app.use(helmet());
-app.use(cors());
+// app.use(helmet());
+// app.use(cors());
 // app.use(express.json());
 // app.use("/apis", itemsRouter);
 // app.use(errorHandler);
