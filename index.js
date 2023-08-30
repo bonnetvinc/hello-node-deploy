5feb56816d55c231870174c8cdd88865c86691a7
Vincent <vincent.vbonnet@gmail.com>
2023-08-30 00:07:01 -0400

    re

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // Add Express
 const express = require("express");
+import cors from "cors";
+import helmet from "helmet";
+import { itemsRouter } from "./items/items.router";
+import { errorHandler } from "./middleware/error.middleware";
+import { notFoundHandler } from "./middleware/not-found.middleware";
 
 // Initialize Express
 const app = express();
@@ -9,11 +14,21 @@ app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
 
+/**
+ *  App Configuration
+ */
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use("/api/menu/items", itemsRouter);
+app.use(errorHandler);
+app.use(notFoundHandler);
+
 // Initialize server
 app.listen(5000, () => {
   console.log("Running on port 5000.");
 });
-
 
 // Export the Express API
 module.exports = app;
---
+++ src/index.js
@@ -0,0 +1,42 @@
+
+import express from "express";
+// import cors from "cors";
+// import helmet from "helmet";
+// import { itemsRouter } from "./items/items.router";
+// import { errorHandler } from "./middleware/error.middleware";
+// import { notFoundHandler } from "./middleware/not-found.middleware";
+
+
+/**
+ * App Variables
+ */
+const PORT = 5000;
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hey this is my API running 🥳')
+})
+
+
+/**
+ *  App Configuration
+ */
+
+// app.use(helmet());
+// app.use(cors());
+// app.use(express.json());
+// app.use("/api/menu/items", itemsRouter);
+// app.use(errorHandler);
+// app.use(notFoundHandler);
+/**
+ * Server Activation
+ */
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
+
+
+
+// Export the Express API
+module.exports = app;
